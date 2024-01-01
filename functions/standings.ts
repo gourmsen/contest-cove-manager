d6febd218836d3e89c52028ce58e34f1f75dd477
Maximilian Niessl <contact@maxniessl.com>
2024-01-01 16:11:07 +0100

    Added points and places to attendee list

---
+++ functions/standings.ts
@@ -0,0 +1,97 @@
+module.exports = {
+    calculatePoints(contestId: string, attendeeId: string, round: number) {
+        let db = require('../functions/database.ts');
+    
+        // query entries and objectives for points
+        let attendeePoints;
+        if (round) {
+            attendeePoints = db.queryDatabase(
+                `SELECT sum(a.objectiveValue * b.value) AS points
+                FROM contest_attendee_entries AS a
+                INNER JOIN contest_objectives AS b
+                ON a.contestId = b.contestId
+                AND a.objectiveName = b.name
+                WHERE a.contestId = ?
+                AND a.attendeeId = ?
+                AND a.round = ?`,
+                [contestId, attendeeId, round]);
+        } else {
+            attendeePoints = db.queryDatabase(
+                `SELECT sum(a.objectiveValue * b.value) AS points
+                FROM contest_attendee_entries AS a
+                INNER JOIN contest_objectives AS b
+                ON a.contestId = b.contestId
+                AND a.objectiveName = b.name
+                WHERE a.contestId = ?
+                AND a.attendeeId = ?`,
+                [contestId, attendeeId]);
+        }
+
+        // check points
+        if (attendeePoints[0].points) {
+            return Math.round(attendeePoints[0].points * 100 / 100);
+        } else {
+            return 0;
+        }
+    },
+    calculatePlace(contestId: string, attendeeId: string, round: number) {
+        let db = require('../functions/database.ts');
+
+        // query entries and objectives for points
+        let attendeePoints;
+        if (round) {
+            attendeePoints = db.queryDatabase(
+                `SELECT a.attendeeId, sum(a.objectiveValue * b.value) AS points
+                FROM contest_attendee_entries AS a
+                INNER JOIN contest_objectives AS b
+                ON a.contestId = b.contestId
+                AND a.objectiveName = b.name
+                WHERE a.contestId = ?
+                AND a.round = ?
+                GROUP BY a.attendeeId
+                ORDER BY points DESC`,
+                [contestId, round]);
+        } else {
+            attendeePoints = db.queryDatabase(
+                `SELECT a.attendeeId, sum(a.objectiveValue * b.value) AS points
+                FROM contest_attendee_entries AS a
+                INNER JOIN contest_objectives AS b
+                ON a.contestId = b.contestId
+                AND a.objectiveName = b.name
+                WHERE a.contestId = ?
+                GROUP BY a.attendeeId
+                ORDER BY points DESC`,
+                [contestId]);
+        }
+
+        // prepare attendees
+        let attendees: any[] = [];
+        for (let i = 0; i < attendeePoints.length; i++) {
+            let attendeeData = {
+                id: attendeePoints[i].attendeeId,
+                points: Math.round(attendeePoints[i].points * 100 / 100)
+            }
+
+            attendees.push(attendeeData);
+        }
+
+        // calculate place
+        let currentPlace = 0;
+        for (let i = 0; i < attendees.length; i++) {
+
+            // set current place
+            if (i) {
+                if (attendees[i].points < attendees[i - 1].points) {
+                    currentPlace = i + 1;
+                }
+            } else {
+                currentPlace = i + 1;
+            }
+
+            // pick out attendee
+            if (attendees[i].id === attendeeId) {
+                return currentPlace;
+            }
+        }
+    }
+}
--- requests/contest-attendee-list.ts
+++ requests/contest-attendee-list.ts
@@ -9,6 +9,24 @@ let payload: any;
 module.exports = {
     listContestAttendees(contestId: string) {
         let db = require('../functions/database.ts');
+        let standings = require('../functions/standings.ts');
+
+        // query contest for contestId
+        let contests = db.queryDatabase(
+            `SELECT *
+            FROM contests
+            WHERE contestId = ?`,
+            [contestId]);
+
+        // check existing contest
+        if (!contests.length) {
+            status = 404;
+            payload = {
+                message: "Contest doesn't exist."
+            }
+
+            return [status, payload];
+        }
 
         // query attendees for contestId
         let contestAttendees = db.queryDatabase(
@@ -29,11 +47,40 @@ module.exports = {
         }
 
         // fill contest attendee list with attendees
-        let contestAttendeeList:ContestAttendeeSchema[] = new Array<ContestAttendeeSchema>();
+        let contestAttendeeList: ContestAttendeeSchema[] = new Array<ContestAttendeeSchema>();
 
         for (let i = 0; i < contestAttendees.length; i++) {
+
+            // query users for attendeeId
+            let users = db.queryDatabase(
+                `SELECT *
+                FROM users
+                WHERE userId = ?`,
+                [contestAttendees[i].attendeeId]);
+            
+            // fill name for attendeeId
+            let name = "";
+            if (users.length) {
+                name = users[0].name
+            }
+
+            // calculate points for each round
+            let points: number[] = [];
+            for (let j = 0; j < contests[0].currentRound + 1; j++) {
+                points[j] = Math.round(standings.calculatePoints(contestId, contestAttendees[i].attendeeId, j) * 100 / 100)
+            }
+
+            // calculate places for each round
+            let places: number[] = [];
+            for (let j = 0; j < contests[0].currentRound + 1; j++) {
+                places[j] = standings.calculatePlace(contestId, contestAttendees[i].attendeeId, j)
+            }
+
             let contestAttendee: ContestAttendeeSchema = {
-                attendeeId: contestAttendees[i].attendeeId
+                attendeeId: contestAttendees[i].attendeeId,
+                name: name,
+                points: points,
+                places: places
             }
 
             contestAttendeeList.push(contestAttendee);
